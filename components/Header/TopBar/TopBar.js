68827f625daf82e2cfdbc5dce2a30ae9103666fd
rafael <rafaelsistem87@gmail.com>
2021-10-19 07:04:57 -0700

    topbar

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,9 +1,40 @@
-const TopBar = () => {
-      return (
-            <div>
-                  <h1>top bar</h1>
-            </div>
-      )
+
+import { Container, Grid, Image, Input } from "semantic-ui-react";
+import Link from "next/link";
+
+export default function TopBar() {
+  return (
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+          <Grid.Column width={8} className="top-bar__left">
+            <Logo />
+          </Grid.Column>
+          <Grid.Column width={8} className="top-bar__right">
+            <Search />
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
-export default TopBar
+function Logo() {
+  return (
+    <Link href="/">
+      <a>
+        <Image src="/logo.png" alt="Gaming" />
+      </a>
+    </Link>
+  );
+}
+
+function Search() {
+  return (
+    <Input
+      id="search-game"
+      icon={{ name: "search" }}
+    />
+  );
+}
+
